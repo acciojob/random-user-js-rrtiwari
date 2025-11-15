50fb9ba98b01d35850f3e7fe4acc2b2a0bb86585
Rahul Tiwari <128848653+rrtiwari@users.noreply.github.com>
2025-11-15 08:09:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-//your code here
+let userData = [];
+
+const photo = document.getElementById("photo");
+const user = document.getElementById("user");
+const info = document.getElementById("info");
+
+const ageBtn = document.getElementById("age");
+const emailBtn = document.getElementById("email");
+const phoneBtn = document.getElementById("phone");
+const otherBtn = document.getElementById("other");
+
+async function fetchData() {
+    let res = await fetch("https://randomuser.me/api/");
+    let json = await res.json();
+    userData = json.results;
+    displayMainInfo();
+}
+
+function displayMainInfo() {
+    const u = userData[0];
+    photo.src = u.picture.large;
+    user.innerText = `${u.name.first} ${u.name.last}`;
+    info.innerText = "";
+}
+
+ageBtn.addEventListener("click", () => {
+    info.innerText = `Age: ${userData[0].dob.age}`;
+});
+
+emailBtn.addEventListener("click", () => {
+    info.innerText = `Email: ${userData[0].email}`;
+});
+
+phoneBtn.addEventListener("click", () => {
+    info.innerText = `Phone: ${userData[0].phone}`;
+});
+
+otherBtn.addEventListener("click", () => {
+    fetchData();
+});
+
+fetchData();
+
+
